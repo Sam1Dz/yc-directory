16aa52adcb4820617c0d6521865aea5751d797b2
Pratama Dimas <pratamadimas.pw@gmail.com>
2025-07-12 11:03:24 +0700

    Update ESLint configuration and dependencies
    - Refactored ESLint configuration to use an array format and added ignore patterns for specific directories.
    - Updated package version from 2025.711.6 to 2025.712.7.
    - Upgraded dependencies:
      - @tanstack/react-router from ^1.125.6 to ^1.127.3
      - @tanstack/react-start from ^1.126.1 to ^1.127.4
      - zod from ^4.0.2 to ^4.0.5
      - @eslint/js and eslint from ^9.30.1 to ^9.31.0.
    
    Enhance NavHeader component with Drawer for user menu
    - Replaced Menu component with Drawer for better mobile experience.
    - Added new buttons for Profile and Create actions within the Drawer.
    - Improved styling and interaction feedback for the profile drawer button.
    
    Update CSS for profile drawer button interaction
    - Changed CSS class from .menuButton to .profileDrawer for active state styling.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,20 +3,32 @@
 import eslint from '@eslint/js';
 import tslint from 'typescript-eslint';
 
-export default tslint.config(
-  eslint.configs.recommended,
-  tslint.configs.recommended,
-  tslint.configs.stylistic,
+export default [
   {
-    rules: {
-      'no-console': ['warn', { allow: ['error', 'warn'] }],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        },
-      ],
-    },
+    ignores: [
+      '**/node_modules/**',
+      '**/.output/**',
+      '**/.tanstack/**',
+      '**/.nitro/**',
+      '**/dist/**',
+      '**/build/**',
+    ],
   },
-);
+  ...tslint.config(
+    eslint.configs.recommended,
+    tslint.configs.recommended,
+    tslint.configs.stylistic,
+    {
+      rules: {
+        'no-console': ['warn', { allow: ['error', 'warn'] }],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ),
+];
--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { TbLogout2 } from 'react-icons/tb';
-import { useMediaQuery } from '@mantine/hooks';
+import { TbLogout2, TbPlus, TbUser } from 'react-icons/tb';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import {
   Avatar,
   Button,
+  type ButtonProps,
+  Divider,
+  Drawer,
   Group,
-  Menu,
   Stack,
   Text,
   UnstyledButton,
 } from '@mantine/core';
 
 import styles from '~/styles/components/header.module.css';
-import { mantineBreakpoints } from '~/libs/mantine/breakpoint';
 import { useAuth } from '~/libs/auth';
+import { mantineBreakpoints } from '~/libs/mantine/breakpoint';
 
 interface UserMenuProps {
   user: {
@@ -23,41 +25,92 @@ interface UserMenuProps {
   };
 }
 
+const menuButtonProps: ButtonProps = {
+  variant: 'subtle',
+  justify: 'flex-start',
+  px: 8,
+  py: 6,
+};
+
 export function NavHeader(props: UserMenuProps) {
   const { user } = props;
 
   const { logout } = useAuth();
+  const [opened, { open, close }] = useDisclosure(false);
   const isMobile = useMediaQuery(`(max-width: ${mantineBreakpoints.sm})`);
 
   return (
-    <Group align="center" gap="lg">
-      {!isMobile && <Button variant="light">Create</Button>}
-      <Menu shadow="md" width={200}>
-        <Menu.Target>
-          <UnstyledButton className={styles.menuButton}>
-            <Group gap="xs" align="center">
-              <Avatar src={user.image} size={36} />
-            </Group>
-          </UnstyledButton>
-        </Menu.Target>
+    <React.Fragment>
+      <Drawer
+        radius="lg"
+        position="right"
+        opened={opened}
+        onClose={close}
+        size={!isMobile ? 'sm' : 'xs'}
+        overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+        styles={{
+          body: {
+            height: 'calc(100% - 72.6px)',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          },
+        }}
+        title={
+          <Group gap="xs" wrap="nowrap">
+            <Avatar src={user.image} size={36} />
+            <Stack gap={0}>
+              <Text size="sm" fw={600} lineClamp={1}>
+                {user.username}
+              </Text>
+              <Text c="gray.6" size="sm" lineClamp={1}>
+                {user.name}
+              </Text>
+            </Stack>
+          </Group>
+        }
+      >
+        <Stack gap={0}>
+          <Divider mb={16} />
+          {isMobile && (
+            <Button
+              {...menuButtonProps}
+              color="white"
+              leftSection={<TbPlus size={20} />}
+            >
+              <Text fw={400}>Create</Text>
+            </Button>
+          )}
+          <Button
+            {...menuButtonProps}
+            color="white"
+            leftSection={<TbUser size={20} />}
+          >
+            <Text fw={400}>Profile</Text>
+          </Button>
+        </Stack>
 
-        <Menu.Dropdown>
-          <Stack gap={0} p="sm">
-            <Text fw={600} size="sm">
-              {user.username}
-            </Text>
-            <Text size="xs">{user.name}</Text>
-          </Stack>
-          <Menu.Divider />
-          <Menu.Item
+        <Stack gap={0} mt={16}>
+          <Divider mb={16} />
+          <Button
+            {...menuButtonProps}
             color="red"
-            leftSection={<TbLogout2 size={14} />}
+            leftSection={<TbLogout2 size={20} />}
             onClick={logout}
           >
-            Logout
-          </Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
-    </Group>
+            <Text fw={400}>Logout</Text>
+          </Button>
+        </Stack>
+      </Drawer>
+
+      <Group align="center" gap="lg">
+        {!isMobile && <Button variant="light">Create</Button>}
+        <UnstyledButton onClick={open} className={styles.profileDrawer}>
+          <Group gap="xs" align="center">
+            <Avatar src={user.image} size={36} />
+          </Group>
+        </UnstyledButton>
+      </Group>
+    </React.Fragment>
   );
 }
